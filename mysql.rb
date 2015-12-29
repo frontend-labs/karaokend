841269291a806ac5d5dd52f23a3d0bc99f40ec50
Jan Sanchez <joejansanchez@gmail.com>
2015-12-28 23:02:59 -0500

    chore(project): actualizo mysql

--- mysql.rb
+++ mysql.rb
@@ -3,22 +3,22 @@
 require "mysql"
 
 begin
-    
-    con = Mysql.new 'localhost', 'root', '14a4fbfb8f489ada', 'karaokend'
+
+    con = Mysql.new 'localhost', 'root', '12022009', 'karaokend'
 
     rs = con.query("SELECT * FROM song")
     n_rows = rs.num_rows
-    
+
     puts "There are #{n_rows} rows in the result set"
-    
+
     n_rows.times do
         puts rs.fetch_row.join("\s")
     end
- 
+
 rescue Mysql::Error => e
     puts e.errno
     puts e.error
-    
+
 ensure
     con.close if con
 end
